fb20ab900ddf36b501903ce3301b444961cd191f
Demian <d@aletheia.icu>
2021-11-27 01:54:48 +0200

    fix multiple databases support

--- pg2tg.py
+++ pg2tg.py
@@ -3,11 +3,13 @@ import glob
 import sys
 import asyncio
 from pyrogram import Client
+from pyrogram.session import Session
 
+
+Session.notice_displayed = True
 
 bot_token = os.environ.get('BOT_TOKEN') or sys.exit('BOT_TOKEN environment variable is empty!')
 chat_id = int(os.environ.get('CHAT_ID') or sys.exit('CHAT_ID environment variable is empty!'))
-
 
 app = Client(
     session_name='py2tg',
@@ -17,6 +19,7 @@ app = Client(
     no_updates=True,
 )
 
+
 async def main():
     files = glob.glob(f'/backups/daily/*.sql.gz')
     if not files:
@@ -24,11 +27,13 @@ async def main():
 
     async with app:
         try:
-            file = max(files, key=os.path.getctime)
-            await app.send_document(chat_id=chat_id, document=file)
-            print('Uploaded to Telegram.')
+            for db in os.environ.get('POSTGRES_DB').split(','):
+                file = max(filter(lambda f: db in f, files), key=os.path.getctime)
+                await app.send_document(chat_id=chat_id, document=file)
+                print(f"'{file}' is uploaded to Telegram.")
         except Exception as e:
             print(e)
 
 
-asyncio.get_event_loop().run_until_complete(main())
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
